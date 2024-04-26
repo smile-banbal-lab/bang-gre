9b32fa25e03d86f81bfbbdc9ceb8a2ba06de48bf
hayeong-yu <hayoung94100@gmail.com>
2024-04-26 14:33:47 +0900

    Main.js - update/Main.css/Searchbar.js/Commons.css-create

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,32 @@
+import React from 'react';
+import Searchbar from "../components/commons/Searchbar";
+import "./Main.css";
 function Main() {
 
 	return (
-		<>
-			<h1>메인 화면</h1>
-		</>
+		<div id="mainContainer">
+            <section id="section01">
+            <img src={process.env.PUBLIC_URL + '/images/Main-front-text.png'} alt="Main Front Text" />
+                <div className="main-search-bar">
+                    <Searchbar/>
+                </div>
+                <div className='main-hash-tag'>
+                   <div className='main-hash-tag-item'>#바나나맛</div>
+                   <div className='main-hash-tag-item'>#우유</div>
+                   <div className='main-hash-tag-item'>#아이스크림</div>
+                   <div className='main-hash-tag-item'>#MD추천</div>
+                   <div className='main-hash-tag-item'>#여름추천</div>
+                </div>
+            </section>
+            <section id="section02">
+                <div id='main-category-box'>
+                    <div className='category-box-item'></div>
+                </div>
+            </section>
+            <section id="section03">
+
+            </section>
+		</div>
 	);
 }
 
