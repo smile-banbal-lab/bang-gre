9ae3d36617f7747b26b8e5216747a03eb1bdd198
DESKTOP-OSFKUPN <chdwp981016@gmail.com>
2024-04-26 09:47:57 +0900

    add: imgs 업로드,  docs: product.json 수정
    product 에 데이터 이름을 묶어서 menu로 넣고 img파일 새로 업로드

--- src/components/items/Menu.js
+++ src/components/items/Menu.js
@@ -19,11 +19,11 @@ function Menu({ id }) {
 	return (
 		menu && (
 			<>
-				<h3>메뉴 이름 : {menu.menuName}</h3>
-				<h3>메뉴 가격 : {menu.menuPrice}</h3>
-				<h3>메뉴 종류 : {menu.categoryName}</h3>
-				<h3>메뉴 상세 : {menu.detail.description}</h3>
-				<img src={menu.detail.image} style={{ maxWidth: 500 }} alt={menu.menuName} />
+				<h3>메뉴 이름 : {menu.name}</h3>
+				<h3>메뉴 가격 : {menu.price}</h3>
+				<h3>메뉴 종류 : {menu.category.type}</h3>
+				<h3>메뉴 상세 : {`'염분: '${menu.Information.Sodium}`}</h3>
+				<img src={menu.image} style={{ maxWidth: 500 }} alt={menu.name} />
 			</>
 		)
 	);
