7f6f59afb10ed4f7082826305faac305f1cc732e
hayeong-yu <hayoung94100@gmail.com>
2024-04-26 20:13:22 +0900

    searchbar/MenuDetail update commit

--- src/components/commons/Searchbar.js
+++ src/components/commons/Searchbar.js
@@ -1,12 +1,21 @@
-import "./Commons.css"
-function Searchbar() {
+import React, { useState } from 'react'; // useState 가져오기
+import "../lists/MenuList"
+import "./Commons.css";
 
-	return (
-		<div id="Searchbar">
-            <input id="menu-search-input" />
-            <button id="menu-search-button">SEARCH</button>
+function Searchbar() {
+    const [searchValue, setSearchValue] = useState(''); // useState 사용
+
+    return (
+        <div id="Searchbar">
+            <input 
+                id="menu-search-input"
+                type="search"
+                placeholder="검색어를 입력해주세요"
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)} />
+            <button id="menu-search-button" >SEARCH</button>
         </div>
-	);
+    );
 }
 
-export default Searchbar;
+export default Searchbar;s
