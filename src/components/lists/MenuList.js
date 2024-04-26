dfe902f77996929d3793000ae29a9acc35513d28
DESKTOP-OSFKUPN <chdwp981016@gmail.com>
2024-04-26 11:40:39 +0900

    feat: menulist -> categorybar html 생성
    기능 구현 안됨

--- src/components/lists/MenuList.js
+++ src/components/lists/MenuList.js
@@ -23,9 +23,33 @@ function MenuList() {
 
 	return (
 		menuList && (
+			<>
+			<div>
+                    <input type="checkbox" />
+                    <label >아이스크림</label>
+                    <input type="checkbox"  />
+                    <label >우유</label>
+                    <input type="checkbox" />
+                    <label >발효유</label>
+                    <input type="checkbox" />
+                    <label >커피</label>
+                    <input type="checkbox" />
+                    <label >주스</label>
+                    <input type="checkbox" />
+                    <label >음료</label>
+                    
+                    <input 
+                        type='search' 
+                        name="menuName" 
+                        placeholder={'메뉴 이름을 입력해주세요'}>
+                        {/* onChange={e => setSearchValue(e.target.value)} */}
+                    </input>
+					<button>검색</button>
+                </div>
 			<div className="menuBox">
 				{menuList.map(menu => <MenuItem key={menu.id} menu={menu} />)}
 			</div>
+			</>
 		)
 	);
 }
