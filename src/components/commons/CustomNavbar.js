e1f660dc1c4451b176f93d2a12ff44efc3de4e79
hayeong-yu <hayoung94100@gmail.com>
2024-04-29 15:32:04 +0900

    bootstrap 사용 - customeNav 구현 - layout에서 <Nav/> 를 <customeNav/>변경 App.js 에서 bootstrap.css 활성화

---
+++ src/components/commons/CustomNavbar.js
@@ -0,0 +1,52 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { resetLoginUser } from "../../modules/UserModule";
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function CustomeNavbar() {
+
+	const isAuthorized = !!sessionStorage.getItem('isLogin');
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
+	const user = useSelector(state => state.userReducer);
+
+	const { id } = user;
+
+	/* 로그아웃 호출 시: localStorage 저장 값 삭제, userReducer 값 리셋, 루트로 이동 */
+	const logoutHandler = () => {
+		sessionStorage.removeItem('isLogin');
+		sessionStorage.removeItem('isVerify');
+		dispatch(resetLoginUser());
+		navigate('/');
+	}
+
+	return (
+		<>
+			<Navbar bg="light" data-bs-theme="light">
+				<Container>
+					<Navbar.Brand as={NavLink} to='/'>BANG-GRE</Navbar.Brand>
+					<Nav className="me-auto">
+						<Nav.Link as={NavLink} to='/'>Home</Nav.Link>
+						<Nav.Link as={NavLink} to='/menu'>Features</Nav.Link>
+						<Nav.Link as={NavLink} to='/'>QnA</Nav.Link>
+					</Nav>
+					{!isAuthorized ? (
+						<Nav.Link as={NavLink} to='/login'>LOGIN</Nav.Link>
+					) : (
+						<NavDropdown title="MyPage" id="basic-nav-dropdown">
+							<NavDropdown.Item as={NavLink} to={`/user/passwordcheck/${id}`}>MyPage</NavDropdown.Item>
+							<NavDropdown.Item onClick={logoutHandler}>LogOut</NavDropdown.Item>
+						</NavDropdown>
+					)
+					}
+				</Container>
+			</Navbar>
+		</>
+	);
+}
+
+export default CustomeNavbar;
--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,13 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/commons/Header';
-import Navbar from '../components/commons/Navbar';
+import CustomeNavbar from '../components/commons/CustomNavbar';
 
 function Layout() {
 
 	return (
 		<div>
 			<Header />
-			<Navbar />
+			<CustomeNavbar />
 			<Outlet />
 		</div>
 	);
