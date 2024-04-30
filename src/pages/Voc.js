765b663195463ed95f98a63ebc44b89a99ffaa80
DESKTOP-OSFKUPN <chdwp981016@gmail.com>
2024-04-30 16:33:31 +0900

    feat: 게시글 작성 버튼 추가
    Form으로 이동

--- src/pages/Voc.js
+++ src/pages/Voc.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { callGetqnaListAPI } from '../apis/QnaAPICalls';
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from "react-router-dom";
 
 import CommonTable from '../components/table/CommonTable';
 import CommonTableColumn from '../components/table/CommonTableColumn';
@@ -11,6 +12,8 @@ function Voc() {
 	const result = useSelector(state => state.qnaReducer);
 	const qnaList = result.qnalist;
 	const dispatch = useDispatch();
+    const isAuthorized = !!sessionStorage.getItem('isLogin');
+    const navigate = useNavigate();
 
 
 	useEffect(() => {
@@ -27,7 +30,7 @@ function Voc() {
 			<CommonTableColumn>{item.id}</CommonTableColumn>
 			<CommonTableColumn>{item.title}</CommonTableColumn>
 			<CommonTableColumn>{item.date}</CommonTableColumn>
-			<CommonTableColumn>{item.user_id}</CommonTableColumn>
+			<CommonTableColumn>{item.userid}</CommonTableColumn>
 			<CommonTableColumn>{item.content}</CommonTableColumn>
 		</CommonTableRow>
 	));
@@ -39,11 +42,12 @@ function Voc() {
 	return (
 		<>
 			<div id='Voc-page'>
-				<h2 >게시글 목록</h2>
-				<CommonTable headersName={['글번호', '제목', '등록일', '작성자', '내용']}>
+				<h2 >게시글 목록 {(isAuthorized) && 
+                <button onClick={() => navigate(`/contact`)}>게시글 작성하기</button>} </h2>
+
+				<CommonTable headersName={['글번호', '제목', '등록일', '작성자(id)', '내용']}>
 					{items}
 				</CommonTable>
-				<button onClick={inputBoardHandler}>게시글 작성하기</button>
 			</div>
 		</>
 	);
