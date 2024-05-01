f5bb54bce0fc6c8254130ff65d431674c8052ef1
pppoya111 <pppoya111@snu.ac.kr>
2024-05-01 15:17:47 +0900

    주문완료~ 텅 구현, 카트 한글화

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import userReducer from "./UserModule";
 import menuReducer from "./MenuModule";
-import cartReducer from "./cartModule";
+import cartReducer from "./CartModule";
 import qnaReducer from "./QnaModule";
 
 const rootReducer = combineReducers({
